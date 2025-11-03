8bfe926aad7001e252a101f89c0328fcfbdeff83
parsalemi <parsalemi@outlook.com>
2025-11-03 12:47:02 +0330

    fix prod and dev config

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,9 @@ const is_docker = process.env.IS_DOCKER === 'true';
 
 const development = {
   client: 'postgresql',
-  connection: is_docker ? {
-    host: process.env.DOCKER_DB_HOST || process.env.DB_HOST,
-    port: process.env.DOCKER_DB_PORT || process.env.DB_PORT,
-    user: process.env.DOCKER_DB_USER || process.env.DB_USERNAME || process.env.DB_USER,
-    password: process.env.DOCKER_DB_PWD || process.env.DB_PASSWORD,
-    database: process.env.DOCKER_DB_NAME || process.env.DB_NAME,
-  } : {
+  connection: {
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME || process.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
@@ -32,7 +26,13 @@ const development = {
 }
 const production = {
   client: 'postgresql',
-  connection: {
+  connection: is_docker ? {
+    host: process.env.DOCKER_DB_HOST,
+    port: process.env.DOCKER_DB_PORT,
+    user: process.env.DOCKER_DB_USER,
+    password: process.env.DOCKER_DB_PWD,
+    database: process.env.DOCKER_DB_NAME,
+  } : { 
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: `${process.env.DB_PASSWORD}`,
